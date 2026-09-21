26d8cc82fc9bc61847e0f2d0824ae90cbb129e51
Lucky Hermawan Roza <luckyhermawanroza@gmail.com>
2026-09-21 23:46:20 +0700

    Add and run WebsiteSeeder for ~150 websites

---
+++ scratch.php
@@ -0,0 +1,365 @@
+<?php
+$data = <<<EOF
+https://ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://situ.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://esakip.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://data.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://pullahta.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+http://silancar.ciamiskab.go.id/	Disdukcapil
+https://atcs.ciamiskab.go.id	Dinas Perhubungan
+https://asn.ciamiskab.go.id	BKPSDM
+https://simpeg3.ciamiskab.go.id	BKPSDM
+https://talenta.ciamiskab.go.id	BKPSDM
+https://sibangpeci.ciamiskab.go.id	BKPSDM
+https://threec.ciamiskab.go.id	Dinas PPKBP3A
+https://jdih.ciamiskab.go.id	Sekretariat Daerah
+https://simanis.ciamiskab.go.id	Dinas PMPTSP
+https://galuhvirtual.ciamiskab.go.id	Dinas Pariwisata
+https://simungil.ciamiskab.go.id/	Badan Kesatuan Bangsa dan Politik
+https://ukpbj.ciamiskab.go.id	Sekretariat Daerah
+https://siskacampernik.ciamiskab.go.id	Pemkab Ciamis
+https://ppid.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://app-bapenda.ciamiskab.go.id	Badan Pendapatan Daerah
+https://investasi.ciamiskab.go.id	Dinas PMPTSP
+https://sijagal.ciamiskab.go.id	Dinas PRKPLH
+http://esign-cp.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://sasakgaluh.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://portal-media.ciamiskab.go.id	Pemkab Ciamis
+https://setda.ciamiskab.go.id	Sekretariat Daerah
+https://setdprd.ciamiskab.go.id	Sekretariat Dprd
+https://inspektorat.ciamiskab.go.id	Inspektorat
+https://bapperida.ciamiskab.go.id	Bapperida
+https://bkpsdm.ciamiskab.go.id	BKPSDM
+https://bpbd.ciamiskab.go.id	BPBD
+https://dpmd.ciamiskab.go.id	Dinas PMD
+https://dpkp.ciamiskab.go.id	Dinas Pertanian dan ketahanan Pangan
+https://dinkes.ciamiskab.go.id	Dinas Kesehatan
+https://dinsos.ciamiskab.go.id	Dinas Sosial
+https://disbudpora.ciamiskab.go.id	Disbudpora
+https://disdik.ciamiskab.go.id	Dinas Pendidikan
+https://diskominfo.ciamiskab.go.id	Dinas Komunikasi dan Informatika
+https://dishub.ciamiskab.go.id	Dinas Perhubungan
+https://dprkplh.ciamiskab.go.id	Dinas PRKPLH
+https://disnakan.ciamiskab.go.id	Dinas Peternakan dan Perikanan
+https://disnaker.ciamiskab.go.id	Dinas Tenaga Kerja
+https://dkukmpp.ciamiskab.go.id/	Dinas KUKMP
+https://dpmptsp.ciamiskab.go.id	Dinas PMPTSP
+https://dputrp.ciamiskab.go.id/	Dinas PUPRP
+https://kesbangpol.ciamiskab.go.id	Badan Kesatuan Bangsa dan Politik
+https://satpolpp.ciamiskab.go.id	Satuan Polisi Pamong Praja
+https://dispusip.ciamiskab.go.id	Dinas Perpustakaan dan Kearsipan
+https://dppkbpppa.ciamiskab.go.id	Dinas PPKBP3A
+https://dispar.ciamiskab.go.id	Dinas Pariwisata
+https://disdukcapil.ciamiskab.go.id	Disdukcapil
+https://bpkd.ciamiskab.go.id	Badan Pengelolaan Keuangan Daerah
+https://rsud.ciamiskab.go.id	Rumah Sakit Umum Daerah
+https://pkm-ciamis.ciamiskab.go.id	PUSKESMAS CIAMIS
+https://pkm-imbanagara.ciamiskab.go.id	PUSKESMAS IMBANAGARA
+https://pkm-banjarsari.ciamiskab.go.id	PUSKESMAS BANJARSARI
+https://pkm-ciulu.ciamiskab.go.id	PUSKESMAS CIULU
+https://pkm-baregbeg.ciamiskab.go.id	PUSKESMAS BAREGBEG
+https://pkm-cidolog.ciamiskab.go.id	PUSKESMAS CIDOLOG
+https://pkm-cipaku.ciamiskab.go.id	PUSKESMAS CIPAKU
+https://pkm-cieurih.ciamiskab.go.id	PUSKESMAS CIEURIH
+https://pkm-cigayam.ciamiskab.go.id	PUSKESMAS CIGAYAM
+https://pkm-cihaurbeuti.ciamiskab.go.id	PUSKESMAS CIHAURBEUTI
+https://pkm-sukamulya.ciamiskab.go.id	PUSKESMAS SUKAMULYA
+https://pkm-cijeungjing.ciamiskab.go.id	PUSKESMAS CIJEUNGJING
+https://pkm-handapherang.ciamiskab.go.id	PUSKESMAS HANDAPHERANG
+https://pkm-cikoneng.ciamiskab.go.id	PUSKESMAS CIKONENG
+https://pkm-cimaragas.ciamiskab.go.id	PUSKESMAS CIMARAGAS
+https://pkm-cisaga.ciamiskab.go.id	PUSKESMAS CISAGA
+https://pkm-jatinagara.ciamiskab.go.id	PUSKESMAS JATINAGARA
+https://pkm-kawali.ciamiskab.go.id	PUSKESMAS KAWALI
+https://pkm-kawalimukti.ciamiskab.go.id	PUSKESMAS KAWALIMUKTI
+https://pkm-kertahayu.ciamiskab.go.id	PUSKESMAS KERTAHAYU
+https://pkm-lakbok.ciamiskab.go.id	PUSKESMAS LAKBOK
+https://pkm-lumbung.ciamiskab.go.id	PUSKESMAS LUMBUNG
+https://pkm-pamarican.ciamiskab.go.id	PUSKESMAS PAMARICAN
+https://pkm-panawangan.ciamiskab.go.id	PUSKESMAS PANAWANGAN
+https://pkm-gardujaya.ciamiskab.go.id	PUSKESMAS GARDUJAYA
+https://pkm-panjalu.ciamiskab.go.id	PUSKESMAS PANJALU
+https://pkm-panumbangan.ciamiskab.go.id	PUSKESMAS PANUMBANGAN
+https://pkm-payungsari.ciamiskab.go.id	PUSKESMAS PAYUNGSARI
+https://pkm-purwadadi.ciamiskab.go.id	PUSKESMAS PURWADADI
+https://pkm-sidaharja.ciamiskab.go.id	PUSKESMAS SIDAHARJA
+https://pkm-rajadesa.ciamiskab.go.id	PUSKESMAS RAJADESA
+https://pkm-rancah.ciamiskab.go.id	PUSKESMAS RANCAH
+https://pkm-sadananya.ciamiskab.go.id	PUSKESMAS SADANANYA
+https://pkm-sindangkasih.ciamiskab.go.id	PUSKESMAS SINDANGKASIH
+https://pkm-sukadana.ciamiskab.go.id	PUSKESMAS SUKADANA
+https://pkm-sukamantri.ciamiskab.go.id	PUSKESMAS SUKAMANTRI
+https://pkm-tambaksari.ciamiskab.go.id	PUSKESMAS TAMBAKSARI
+https://kecamatan-ciamis.ciamiskab.go.id	Kecamatan Ciamis
+https://kecamatan-baregbeg.ciamiskab.go.id	Kecamatan Baregbeg
+https://kecamatan-cijeungjing.ciamiskab.go.id	Kecamatan Cijeungjing
+https://kecamatan-sadananya.ciamiskab.go.id	Kecamatan Sadananya
+https://kecamatan-cikoneng.ciamiskab.go.id	Kecamatan Cikoneng
+https://kecamatan-sukadana.ciamiskab.go.id	Kecamatan Sukadana
+https://kecamatan-sindangkasih.ciamiskab.go.id	Kecamatan Sindangkasih
+https://kecamatan-cisaga.ciamiskab.go.id	Kecamatan Cisaga
+https://kecamatan-cipaku.ciamiskab.go.id	Kecamatan Cipaku
+https://kecamatan-cimaragas.ciamiskab.go.id	Kecamatan Cimaragas
+https://kecamatan-cihaurbeuti.ciamiskab.go.id	Kecamatan Cihaurbeuti
+https://kecamatan-cidolog.ciamiskab.go.id	Kecamatan Cidolog
+https://kecamatan-kawali.ciamiskab.go.id	Kecamatan Kawali
+https://kecamatan-jatinagara.ciamiskab.go.id	Kecamatan Jatinagara
+https://kecamatan-rancah.ciamiskab.go.id	Kecamatan Rancah
+https://kecamatan-lumbung.ciamiskab.go.id	Kecamatan Lumbung
+https://kecamatan-rajadesa.ciamiskab.go.id	Kecamatan Rajadesa
+https://kecamatan-panawangan.ciamiskab.go.id	Kecamatan Panawangan
+https://kecamatan-panumbangan.ciamiskab.go.id	Kecamatan Panumbangan
+https://kecamatan-panjalu.ciamiskab.go.id	Kecamatan Panjalu
+https://kecamatan-lakbok.ciamiskab.go.id	Kecamatan Lakbok
+https://kecamatan-banjarsari.ciamiskab.go.id	Kecamatan Banjarsari
+https://kecamatan-pamarican.ciamiskab.go.id	Kecamatan Pamarican
+https://kecamatan-sukamantri.ciamiskab.go.id	Kecamatan Sukamantri
+https://kecamatan-tambaksari.ciamiskab.go.id	Kecamatan Tambaksari
+https://kecamatan-banjaranyar.ciamiskab.go.id	Kecamatan Banjaranyar
+https://kecamatan-purwadadi.ciamiskab.go.id	Kecamatan Purwadadi
+https://kelurahan-ciamis.ciamiskab.go.id	KELURAHAN CIAMIS
+https://kelurahan-maleber.ciamiskab.go.id	KELURAHAN MALEBER
+https://kelurahan-sindangrasa.ciamiskab.go.id	KELURAHAN SINDANGRASA
+https://kelurahan-kertasari.ciamiskab.go.id	KELURAHAN KERTASARI
+https://kelurahan-linggasari.ciamiskab.go.id	KELURAHAN LINGGASARI
+https://kelurahan-benteng.ciamiskab.go.id	KELURAHAN BENTENG
+https://kelurahan-cigembor.ciamiskab.go.id	KELURAHAN CIGEMBOR
+https://pawindan.desa.id	PAWINDAN
+https://cisadap.desa.id	CISADAP
+https://panaragan.desa.id	PANARAGAN
+https://nasol.desa.id	NASOL
+https://gegempalan.desa.id	GEGEMPALAN
+https://cihalarang.desa.id	CIHALARANG
+https://karangkamulyan.desa.id	KARANGKAMULYAN
+https://kertabumi.desa.id	KERTABUMI
+https://cijeungjing.desa.id	CIJEUNGJING
+https://pamalayan-ciamis.desa.id	PAMALAYAN-CIAMIS
+https://utama.desa.id	UTAMA
+https://kertaharja-cijeungjing.desa.id	KERTAHARJA-CIJEUNGJING
+https://karanganyar-cijeungjing.desa.id	KARANGANYAR-CIJEUNGJING
+https://cidolog.desa.id	CIDOLOG
+https://janggala.desa.id	JANGGALA
+https://hegarmanah-cidolog.desa.id	HEGARMANAH-CIDOLOG
+https://jelegong-cidolog.desa.id	JELEGONG-CIDOLOG
+https://ciparay-cidolog.desa.id	CIPARAY-CIDOLOG
+https://sukasari-cidolog.desa.id	SUKASARI-CIDOLOG
+https://sukamulya-chb.desa.id	SUKAMULYA-CHB
+https://sukahurip-cihaurbeuti.desa.id	SUKAHURIP-CIHAURBEUTI
+https://sukamaju-cihaurbeuti.desa.id	SUKAMAJU-CIHAURBEUTI
+https://sukasetia-cihaurbeuti.desa.id	SUKASETIA-CIHAURBEUTI
+https://sumberjaya-cihaurbeuti.desa.id	SUMBERJAYA-CIHAURBEUTI
+https://cihaurbeuti-cihaurbeuti.desa.id	CIHAURBEUTI-CIHAURBEUTI
+https://pasirtamiang.desa.id	PASIRTAMIANG
+https://padamulya-cihaurbeuti.desa.id	PADAMULYA-CIHAURBEUTI
+https://pamokolan.desa.id	PAMOKOLAN
+https://sukahaji-cihaurbeuti.desa.id	SUKAHAJI-CIHAURBEUTI
+https://medanglayang.desa.id	MEDANGLAYANG
+https://panumbangan.desa.id	PANUMBANGAN
+https://golat.desa.id	GOLAT
+https://sindangherang.desa.id	SINDANGHERANG
+https://banjarangsana.desa.id	BANJARANGSANA
+https://payungagung.desa.id	PAYUNGAGUNG
+https://tanjungmulya.desa.id	TANJUNGMULYA
+https://payungsari.desa.id	PAYUNGSARI
+https://jayagiri-pnb.desa.id	JAYAGIRI-PNB
+https://kertaraharja.desa.id	KERTARAHARJA
+https://sindangmukti.desa.id	SINDANGMUKTI
+https://sindangbarang.desa.id	SINDANGBARANG
+https://buanamekar.desa.id	BUANAMEKAR
+https://panjalu.desa.id	PANJALU
+https://ciomas.desa.id	CIOMAS
+https://sandingtaman.desa.id	SANDINGTAMAN
+https://maparah.desa.id	MAPARAH
+https://bahara.desa.id	BAHARA
+https://hujungtiwu.desa.id	HUJUNGTIWU
+https://mandalare.desa.id	MANDALARE
+https://kawali.desa.id	KAWALI
+https://talagasari-kawali.desa.id	TALAGASARI-KAWALI
+https://karangpawitan.desa.id	KARANGPAWITAN
+https://winduraja.desa.id	WINDURAJA
+https://margamulya-kawali.desa.id	MARGAMULYA-KAWALI
+https://purwasari-kawali.desa.id	PURWASARI-KAWALI
+https://sindangsari-kawali.desa.id	SINDANGSARI-KAWALI
+https://kawalimukti.desa.id	KAWALIMUKTI
+https://selasari-kawali.desa.id	SELASARI-KAWALI
+https://panawangan.desa.id	PANAWANGAN
+https://sagalaherang.desa.id	SAGALAHERANG
+https://nagarapageuh.desa.id	NAGARAPAGEUH
+https://nagarajati.desa.id	NAGARAJATI
+https://nagarajaya.desa.id	NAGARAJAYA
+https://kertayasa.desa.id	KERTAYASA
+https://indragiri-panawangan.desa.id	INDRAGIRI-PANAWANGAN
+https://cinyasag-panawangan.desa.id	CINYASAG-PANAWANGAN
+https://sadapaingan.desa.id	SADAPAINGAN
+https://jagabaya-panawangan.desa.id	JAGABAYA-PANAWANGAN
+https://gardujaya.desa.id	GARDUJAYA
+https://karangpaningal-panawangan.desa.id	KARANGPANINGAL-PANAWANGAN
+https://bangunjaya-panawangan.desa.id	BANGUNJAYA-PANAWANGAN
+https://girilaya.desa.id	GIRILAYA
+https://kertajaya-panawangan.desa.id	KERTAJAYA-PANAWANGAN
+https://nagarawangi-panawangan.desa.id	NAGARAWANGI-PANAWANGAN
+https://mekarbuana.desa.id	MEKARBUANA
+https://natanegara.desa.id	NATANEGARA
+https://buniseuri.desa.id	BUNISEURI
+https://selacai.desa.id	SELACAI
+https://jalatrang-cipaku.desa.id	JALATRANG-CIPAKU
+https://cipaku-ciamis.desa.id	CIPAKU-CIAMIS
+https://cieurih-ciamis.desa.id	CIEURIH-CIAMIS
+https://pusakasari.desa.id	PUSAKASARI
+https://bangbayang-cipaku.desa.id	BANGBAYANG-CIPAKU
+https://selamanik.desa.id	SELAMANIK
+https://ciakar.desa.id	CIAKAR
+https://sukawening-cipaku.desa.id	SUKAWENING-CIPAKU
+https://jatinagara.desa.id	JATINAGARA
+https://sukanagara-jatinagara.desa.id	SUKANAGARA-JATINAGARA
+https://cintanagara.desa.id	CINTANAGARA
+https://dayeuhluhur.desa.id	DAYEUHLUHUR
+https://mulyasari-jatinagara.desa.id	MULYASARI-JATINAGARA
+https://bayasari.desa.id	BAYASARI
+https://rajadesa.desa.id	RAJADESA
+https://tanjungsukur.desa.id	TANJUNGSUKUR
+https://tanjungsari-rajadesa.desa.id	TANJUNGSARI-RAJADESA
+https://tanjungjaya.desa.id	TANJUNGJAYA
+https://andapraja.desa.id	ANDAPRAJA
+https://sukaharja-rajadesa.desa.id	SUKAHARJA-RAJADESA
+https://sukajaya-rajadesa.desa.id	SUKAJAYA-RAJADESA
+https://tigaherang.desa.id	TIGAHERANG
+https://sirnabaya.desa.id	SIRNABAYA
+https://sirnajaya-rajadesa.desa.id	SIRNAJAYA-RAJADESA
+https://purwaraja.desa.id	PURWARAJA
+https://sukadana-ciamis.desa.id	SUKADANA-CIAMIS
+https://salakaria.desa.id	SALAKARIA
+https://margaharja.desa.id	MARGAHARJA
+https://margajaya-sukadana.desa.id	MARGAJAYA-SUKADANA
+https://bunter.desa.id	BUNTER
+https://ciparigi.desa.id	CIPARIGI
+https://rancah.desa.id	RANCAH
+https://kiarapayung.desa.id	KIARAPAYUNG
+https://cileungsir.desa.id	CILEUNGSIR
+https://cisontrol.desa.id	CISONTROL
+https://bojonggedang.desa.id	BOJONGGEDANG
+https://karangpari.desa.id	KARANGPARI
+https://dadiharja.desa.id	DADIHARJA
+https://giriharja.desa.id	GIRIHARJA
+https://wangunsari-rancah.desa.id	WANGUNSARI-RANCAH
+https://situmandala.desa.id	SITUMANDALA
+https://kawunglarang.desa.id	KAWUNGLARANG
+https://patakaharja.desa.id	PATAKAHARJA
+https://tambaksari-tambaksari.desa.id	TAMBAKSARI-TAMBAKSARI
+https://mekarsari-tambaksari.desa.id	MEKARSARI-TAMBAKSARI
+https://kaso.desa.id	KASO
+https://kadupandak-tambaksari.desa.id	KADUPANDAK-TAMBAKSARI
+https://sukasari-tambaksari.desa.id	SUKASARI-TAMBAKSARI
+https://karangpaningal-tambaksari.desa.id	KARANGPANINGAL-TAMBAKSARI
+https://sidaharja-lakbok.desa.id	SIDAHARJA-LAKBOK
+https://kertajaya-lakbok.desa.id	KERTAJAYA-LAKBOK
+https://sukanagara-lakbok.desa.id	SUKANAGARA-LAKBOK
+https://cintajaya.desa.id	CINTAJAYA
+https://cintaratu-lakbok.desa.id	CINTARATU-LAKBOK
+https://sindangangin.desa.id	SINDANGANGIN
+https://tambakreja.desa.id	TAMBAKREJA
+https://baregbeg-lakbok.desa.id	BAREGBEG-LAKBOK
+https://kalapasawit.desa.id	KALAPASAWIT
+https://puloerang.desa.id	PULOERANG
+https://kawasen.desa.id	KAWASEN
+https://cicapar.desa.id	CICAPAR
+https://cibadak-ciamis.desa.id	CIBADAK-CIAMIS
+https://banjarsari-banjarsari.desa.id	BANJARSARI-BANJARSARI
+https://sindanghayu.desa.id	SINDANGHAYU
+https://sindangasih-banjarsari.desa.id	SINDANGASIH-BANJARSARI
+https://sindangsari-banjarsari.desa.id	SINDANGSARI-BANJARSARI
+https://ciherang-banjarsari.desa.id	CIHERANG-BANJARSARI
+https://purwasari-banjarsari.desa.id	PURWASARI-BANJARSARI
+https://ratawangi.desa.id	RATAWANGI
+https://ciulu.desa.id	CIULU
+https://pamarican-pamarican.desa.id	PAMARICAN-PAMARICAN
+https://sidamulih.desa.id	SIDAMULIH
+https://sukamukti-pamarican.desa.id	SUKAMUKTI-PAMARICAN
+https://sukajadi-pamarican.desa.id	SUKAJADI-PAMARICAN
+https://sidaharja.desa.id	SIDAHARJA
+https://bangunsari-pamarican.desa.id	BANGUNSARI-PAMARICAN
+https://neglasari-pamarican.desa.id	NEGLASARI-CIAMIS
+https://bantarsari.desa.id	BANTARSARI
+https://margajaya-ciamis.desa.id	MARGAJAYA-CIAMIS
+https://sukahurip.desa.id	Desa Sukahurip
+https://pasirnagara-pmrc.desa.id	PASIRNAGARA-PMRC
+https://cimaragas.desa.id	CIMARAGAS
+https://beber.desa.id	BEBER
+https://raksabaya.desa.id	RAKSABAYA
+https://bojongmalang.desa.id	BOJONGMALANG
+https://jayaraksa.desa.id	JAYARAKSA
+https://cisaga.desa.id	CISAGA
+https://sidamulya-cisaga.desa.id	SIDAMULYA-CISAGA
+https://kepel-cisaga.desa.id	KEPEL-CISAGA
+https://wangunjaya-cisaga.desa.id	WANGUNJAYA-CISAGA
+https://sukahurip-cisaga.desa.id	SUKAHURIP-CISAGA
+https://bangunharja.desa.id	BANGUNHARJA
+https://tanjungjaya-cisaga.desa.id	TANJUNGJAYA-CISAGA
+https://mekarmukti-cisaga.desa.id	MEKARMUKTI-CISAGA
+https://danasari-cisaga.desa.id	DANASARI-CISAGA
+https://girimukti-cisaga.desa.id	GIRIMUKTI-CISAGA
+https://karyamulya.desa.id	KARYAMULYA
+https://sindangkasih.desa.id	SINDANGKASIH
+https://budiasih.desa.id	BUDIASIH
+https://budiharja.desa.id	BUDIHARJA
+https://sukaraja-sindangkasih.desa.id	SUKARAJA-SINDANGKASIH
+https://sukamanah-sinkas.desa.id	SUKAMANAH-SINKAS
+https://sukasenang-sindangkasih.desa.id	SUKASENANG-SINDANGKASIH
+https://sukaresik-sindangkasih.desa.id	SUKARESIK-SINDANGKASIH
+https://wanasigra.desa.id	WANASIGRA
+https://baregbeg.desa.id	BAREGBEG
+https://sukamaju-baregbeg.desa.id	SUKAMAJU-BAREGBEG
+https://saguling.desa.id	SAGULING
+https://petirhilir.desa.id	PETIRHILIR
+https://pusakanagara.desa.id	PUSAKANAGARA
+https://jelat.desa.id	JELAT
+https://karangampel.desa.id	KARANGAMPEL
+https://sukamulya-baregbeg.desa.id	SUKAMULYA-BAREGBEG
+https://sukamantri-sukamantri.desa.id	SUKAMANTRI-SUKAMANTRI
+https://tenggerraharja.desa.id	TENGGERRAHARJA
+https://cibeureum-ciamis.desa.id	CIBEUREUM-CIAMIS
+https://sindanglaya-sukamantri.desa.id	SINDANGLAYA-SUKAMANTRI
+https://mekarwangi-sukamantri.desa.id	MEKARWANGI-SUKAMANTRI
+https://cikupa-lumbung.desa.id	CIKUPA-LUMBUNG
+https://sukaraharja-lumbung-ciamis.desa.id	SUKARAHARJA-LUMBUNG-CIAMIS
+https://lumbung.desa.id	LUMBUNG
+https://lumbungsari-ciamis.desa.id	LUMBUNGSARI-CIAMIS
+https://rawa.desa.id	RAWA
+https://bantardawa.desa.id	BANTARDAWA
+https://purwadadi-purwadadi.desa.id	PURWADADI-PURWADADI
+https://purwajaya-ciamis.desa.id	PURWAJAYA-CIAMIS
+https://karangpaningal.desa.id	KARANGPANINGAL
+https://sidarahayu.desa.id	SIDARAHAYU
+https://padaringan.desa.id	PADARINGAN
+https://pasirlawang.desa.id	PASIRLAWANG
+https://kutawaringin-purwadadi.desa.id	KUTAWARINGIN-PURWADADI
+https://sukamulya-purwadadi.desa.id	SUKAMULYA-PURWADADI
+https://pasawahan-banjaranyar.desa.id	PASAWAHAN-BANJARANYAR
+https://banjaranyar-banjaranyar.desa.id	BANJARANYAR-BANJARANYAR
+https://langkapsari.desa.id	LANGKAPSARI
+https://kalijaya.desa.id	KALIJAYA
+https://cikupa-banjaranyar.desa.id	CIKUPA-BANJARANYAR
+https://karyamukti-ciamis.desa.id	KARYAMUKTI-CIAMIS
+https://cigayam.desa.id	CIGAYAM
+https://sindangrasa.desa.id	SINDANGRASA
+https://tanjungsari-banjaranyar.desa.id	TANJUNGSARI-BANJARANYAR
+https://bapenda.ciamiskab.go.id	Badan Pendapatan Daerah
+https://absensi-diskominfo.ciamiskab.go.id	diskominfo
+https://helo.ciamiskab.go.id/	Helo Ciamis
+https://silancar.ciamiskab.go.id/	Disdukcapil
+EOF;
+
+$lines = explode("\n", trim($data));
+$out = [];
+foreach ($lines as $line) {
+    if (empty(trim($line))) continue;
+    $parts = explode("\t", trim($line));
+    if (count($parts) >= 2) {
+        $url = trim($parts[0]);
+        $author = trim($parts[1]);
+        $name = str_replace(["https://", "http://", "/"], "", $url);
+        $out[] = "            ['url' => '$url', 'name' => '$name', 'author' => '$author'],";
+    }
+}
+file_put_contents('database/seeders/data.txt', implode("\n", $out));
+?>
